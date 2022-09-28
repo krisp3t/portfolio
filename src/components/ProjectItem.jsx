3989dd4d82f3fa547968df29792f3cd4c7a6ae3f
krisp3t <77499501+krisp3t@users.noreply.github.com>
2022-09-28 16:46:11 +0200

    Add ProjectItem hover

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import {SiGithub} from "react-icons/si";
 
 import Tag from "./Tag";
 
 export default function ProjectItem(props) {
     const item = props.item;
+    console.log(<SiGithub/>);
 
     return (<li key={item.id}>
         <a href={item.github}>
-            <img src={`${item.imgPath}`} alt={item.name}
-                 className="w-full object-bottom border-4 mb-4 h-72 object-cover hover:brightness-75 transition-all duration-150"
-            />
+
+            <div
+                className={`mb-4 h-72 w-full bg-gray-500 bg-opacity-10 relative flex items-center justify-center`}>
+                <SiGithub size={70}/>
+                <img src={`${item.imgPath}`} alt={item.name}
+                     className="w-full object-bottom border-4 mb-4 h-72 object-cover hover:brightness-75 hover:opacity-20 transition-opacity duration-150 absolute top-0 left-0"
+                />
+            </div>
         </a>
         {item.tags && item.tags.map((el, ix) => <Tag name={el} key={ix}/>)}
         <a href={item.github}><h3
